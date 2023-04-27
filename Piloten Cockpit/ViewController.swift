fb5c8423c9099c19967a3467e4ef61b12cf6b000
Michael Sommer <michael.sommer@neuronetix.de>
2023-04-27 17:54:05 +0200

    Initial checkin

--- Piloten Cockpit/ViewController.swift
+++ Piloten Cockpit/ViewController.swift
@@ -1,19 +1,48 @@
 //
 //  ViewController.swift
-//  Piloten Cockpit
+//  wkwebview
 //
-//  Created by Administrator on 15.04.23.
+//  Created by Michael Sommer
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func loadView() {
+        
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        
+        webView.navigationDelegate = self
+        view = webView
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // webView.frame = view.bounds            // previously
+        let topPadding = view.safeAreaInsets.top
+        webView.frame = view.frame.inset(by: UIEdgeInsets(top: topPadding, left: CGFloat(0), bottom: CGFloat(0), right: CGFloat(0)))
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "https://pilot.baar-flieger.de/app/benutzer")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+        //view.backgroundColor = .red
+    }
 
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        NSLog("did fail provisional navigation %@", error as NSError)
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        NSLog("did fail navigation %@", error as NSError)
+    }
+    
 }
 
